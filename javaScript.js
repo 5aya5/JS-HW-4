cbf365a811f6c547524b50868135edf0f247c9d6
Said <saidsaid01sm@gmail.com>
2023-12-19 17:19:40 +0500

    first

---
+++ javaScript.js
@@ -0,0 +1,79 @@
+var time = +prompt("Введи время:");
+
+switch (time) {
+    case 0:
+        alert("Сейчас полночь!");
+        break;
+    case 1:
+        alert("Сейчас час ночи!");
+        break;
+    case 2:
+        alert("Сейчас два часа ночи!");
+        break;
+    case 3:
+        alert("Сейчас три часа ночи!");
+        break;
+    case 4:
+        alert("Сейчас четыре часа ночи!");
+        break;
+    case 5:
+        alert("Сейчас пять часов ночи!");
+        break;
+    case 6:
+        alert("Сейчас шесть часов утра!");
+        break;
+    case 7:
+        alert("Сейчас семь часов утра!");
+        break;
+    case 8:
+        alert("Сейчас восемь часов утра!");
+        break;
+    case 9:
+        alert("Сейчас девять часов утра!");
+        break;
+    case 10:
+        alert("Сейчас десять часов утра!");
+        break;
+    case 11:
+        alert("Сейчас одинадцать часов утра!");
+        break;
+    case 12:
+        alert("Сейчас полдень!");
+        break;
+    case 13:
+        alert("Сейчас час дня!");
+        break;
+    case 14:
+        alert("Сейчас два часа дня!");
+        break;
+    case 15:
+        alert("Сейчас три часа дня!");
+        break;
+    case 16:
+        alert("Сейчас четыре часа дня!");
+        break;
+    case 17:
+        alert("Сейчас пять часов дня!");
+        break;
+    case 18:
+        alert("Сейчас шесть часов дня!");
+        break;
+    case 19:
+        alert("Сейчас семь часов вечера!");
+        break;
+    case 20:
+        alert("Сейчас восемь часов вечера!");
+        break;
+    case 21:
+        alert("Сейчас девять часов вечера!");
+        break;
+    case 22:
+        alert("Сейчас десять часов вечера!");
+        break;
+    case 23:
+        alert("Сейчас одинадцать часов вечера!");
+        break;
+    default:
+        alert("Введено некорректное время!");
+        break;
+}
